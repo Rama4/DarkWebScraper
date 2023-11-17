00570cfa1ed7bdccc936d689a7846459503dcd73
sushmitamallick <sushmitamallicksphs@gmail.com>
2023-11-16 17:21:30 -0700

    Updated interface with checkboxes for field names, made UI look better

--- CODE/Interface/main.js
+++ CODE/Interface/main.js
@@ -1,6 +1,14 @@
 const SEARCH_ENDPOINT = "https://darkwebscraper.run-us-west2.goorm.site/myapp/search?"
 const QUERY_PARAM = "q="
 const FIELD_PARAM = "fl="
+    
+let checkbox1 = document.getElementById("checkbox1");
+let checkbox2 = document.getElementById("checkbox2");
+let checkbox3 = document.getElementById("checkbox3");
+let checkbox4 = document.getElementById("checkbox4");
+let checkbox5 = document.getElementById("checkbox5");
+let checkbox6 = document.getElementById("checkbox6");
+let checkbox7 = document.getElementById("checkbox7");
 
 function handleSearch(event) {
     // Get the value from the search bar
@@ -9,15 +17,28 @@ function handleSearch(event) {
     // You can now use the searchTerm variable to perform your search or other actions
     console.log('Search term:', searchTerm);
 
-    queryBackend("name", searchTerm)
+    let searchFields = getAllSearchFields()
+    console.log(searchFields)
+    queryBackend(searchFields, searchTerm)
     // Prevent the form from actually submitting
     event.preventDefault();
 }
 
-function queryBackend(searchField, searchTerm) {
-    var searchUrl = SEARCH_ENDPOINT + QUERY_PARAM + searchField + ":*" + "&" + FIELD_PARAM + "name,id"
+function queryBackend(searchFields, searchTerm) {
+    let queryParam = "("
+    for (var i = 0; i < searchFields.length; i++) {
+        let searchField = searchFields[i]
+        queryParam += searchField + ":" + "*" + searchTerm + "*"
+        if (i == searchFields.length-1) {
+            queryParam += ")"
+        } else {
+            queryParam += " OR "
+        }
+    }
+    console.log("queryParam", queryParam)
+    var searchUrl = SEARCH_ENDPOINT + QUERY_PARAM + queryParam + "&" + FIELD_PARAM + "name,id,quantity,quality,cost_eu,cost_btc"
     console.log("searchUrl", searchUrl)
-    //https://darkwebscraper.run-us-west2.goorm.site/myapp/search?q=name:Bubblegum&fl=title,id
+
     fetch(searchUrl, {
         method: 'GET',
         headers: new Headers({
@@ -42,4 +63,49 @@ function queryBackend(searchField, searchTerm) {
         // Handle any errors here 
         console.error(error); // Example: Logging the error to the console 
     });
+}
+
+function selectAllCheckboxes() {
+    
+    checkbox2.checked = true;
+    checkbox3.checked = true;
+    checkbox4.checked = true;
+    checkbox5.checked = true;
+    checkbox6.checked = true;
+    checkbox7.checked = true;
+}
+
+function reevaluateAll(event) {
+    if (!event.target.checked) {
+        checkbox1.checked = false;
+    }
+}
+
+function getAllSearchFields() {
+    let searchFields = [];
+
+    if (checkbox2.checked) {
+        searchFields.push(checkbox2.value)
+    }
+
+    if (checkbox3.checked) {
+        searchFields.push(checkbox3.value)
+    }
+    
+    if (checkbox4.checked) {
+        searchFields.push(checkbox4.value)
+    }
+
+    if (checkbox5.checked) {
+        searchFields.push(checkbox5.value)
+    }
+
+    if (checkbox6.checked) {
+        searchFields.push(checkbox6.value)
+    }
+
+    if (checkbox7.checked) {
+        searchFields.push(checkbox7.value)
+    }
+    return searchFields;
 }
